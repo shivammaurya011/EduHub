500f575ca31b23bc37ce6efcd0fb6162797db309
vibhamaurya011 <vibhamaurya011@gmail.com>
2024-03-16 13:35:10 +0530

    dotenv  done

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3030/api"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = process.env.BASE_URL || "http://localhost:3030/api";
 
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.timeout = 2500;
 
-
-export default axiosInstance
+export default axiosInstance;
